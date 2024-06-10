02dbe96f5423eb3ccd500fb97160cacda7f49761
Patrick Morton <patrickjamesmorton@gmail.com>
2024-06-10 13:38:55 +0100

    Added a title telling the user to login and labels for the input text boxes, so that the user knows what they need to do

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,7 +5,11 @@ describe('template spec', () => {
 
   it('displays input text fields for user name and password and a login button', () => {
     cy.visit('http://localhost:3000')
+    cy.get('p').contains('Login to your account')
+    cy.get('form').should('exist')
+    cy.get('label').contains('Username')
     cy.get('input[name="username"]').should('exist')
+    cy.get('label').contains('Password')
     cy.get('input[name="password"]').should('exist')
     cy.get('button').contains('Login')
   })
--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="App">
       <form id="login-form">
+        <p>Login to your account:</p>
+        <br />
+        <label>Username:</label>
         <input type="text" id="username" name="username" />
         <br /> <br />
+        <label>Password:</label>
         <input type="password" id="password" name="password" />
         <br /> <br />
         <button id="login">Login</button>
